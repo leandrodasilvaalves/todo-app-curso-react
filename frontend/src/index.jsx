745c41539c01f97f122e837c3ad3e855c550730f
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-02-27 20:50:34 -0300

    Aula 86

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './main/app';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
-ReactDom.render(<App/>, document.getElementById('app'));
+import App from './main/app';
+import reduces from './main/reducers';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reduces, devTools);
+
+ReactDom.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+, document.getElementById('app'));
